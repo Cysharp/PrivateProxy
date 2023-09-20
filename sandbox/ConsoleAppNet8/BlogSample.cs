340d7ef29e3ea6146485dd151480e273087ba9df
neuecc <ils@neue.cc>
2023-09-21 03:30:44 +0900

    gh actions

--- sandbox/ConsoleAppNet8/BlogSample.cs
+++ sandbox/ConsoleAppNet8/BlogSample.cs
@@ -15,65 +15,51 @@ using System.Threading.Tasks;
 
 
 
-//public class Sample
-//{
-//    int _field1;
-//    int _field2;
-//    public int PrivateProperty { get; private set; }
-//    int PrivateAdd(int x, int y) => x + y;
-//}
+public class Sample
+{
+    int _field1;
+    int _field2;
+    public int PrivateProperty { get; private set; }
+    int PrivateAdd(int x, int y) => x + y;
+}
 
-
-//[GeneratePrivateProxy(typeof(Sample))]
-//public partial struct SampleProxy;
-
-
-//public struct MutableStructSample
-//{
-//    int _counter;
-//    void Increment() => _counter++;
-
-//    // static and ref sample
-//    static ref int GetInstanceCounter(ref MutableStructSample sample) => ref sample._counter;
-//}
-
-//// use ref partial struct
-//[GeneratePrivateProxy(typeof(MutableStructSample))]
-//public ref partial struct MutableStructSampleProxy;
-
-
-//public static class Calle
-//{
-//    public static void Foo()
-//    {
-//var sample = new MutableStructSample();
-//var proxy = sample.AsPrivateProxy();
-//proxy.Increment();
-//proxy.Increment();
-//proxy.Increment();
-
-//// call private static method.
-//ref var counter = ref MutableStructSampleProxy.GetInstanceCounter(ref sample);
-
-//Console.WriteLine(counter); // 3
-//counter = 9999;
-//Console.WriteLine(proxy._counter); // 9999
-//    }
-//}
-
-
-
-public class Sample;
-
-// class proxy type both supports class and struct(recommend is struct)
-[GeneratePrivateProxy(typeof(Sample))]
-public partial class SampleProxy;
 
 [GeneratePrivateProxy(typeof(Sample))]
-public partial struct SampleProxy2;
+public partial struct SampleProxy;
 
-public struct SamplleStruct;
 
-// struct only supports ref struct
-[GeneratePrivateProxy(typeof(SamplleStruct))]
-public ref partial struct SamplleStructProxy;
+public struct MutableStructSample
+{
+    int _counter;
+    void Increment() => _counter++;
+
+    // static and ref sample
+    static ref int GetInstanceCounter(ref MutableStructSample sample) => ref sample._counter;
+}
+
+// use ref partial struct
+[GeneratePrivateProxy(typeof(MutableStructSample))]
+public ref partial struct MutableStructSampleProxy;
+
+
+public static class Calle
+{
+    public static void Foo()
+    {
+var sample = new MutableStructSample();
+var proxy = sample.AsPrivateProxy();
+proxy.Increment();
+proxy.Increment();
+proxy.Increment();
+
+// call private static method.
+ref var counter = ref MutableStructSampleProxy.GetInstanceCounter(ref sample);
+
+Console.WriteLine(counter); // 3
+counter = 9999;
+Console.WriteLine(proxy._counter); // 9999
+    }
+}
+
+
+
